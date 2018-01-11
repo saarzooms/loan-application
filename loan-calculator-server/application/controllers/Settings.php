e7d4e50772d82e1159fd6ac3635ecf40550e8fd8
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-01-11 20:15:51 +0530

    Update Settings.php

--- loan-calculator-server/application/controllers/Settings.php
+++ loan-calculator-server/application/controllers/Settings.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Settings extends CI_Controller {
 
-	function Settings(){
-	//	 public function __construct() {
+	//function Settings(){
+	public function __construct() {
 		parent::__construct();
 		$this->load->model('Settings_model');
 		$this->session->set_userdata('userId', 101);
