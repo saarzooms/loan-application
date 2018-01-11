caf7b3963214eee36bedcda25d64953f02d588db
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-01-11 13:37:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 session_start();
-$hostname='us-cdbr-iron-east-05.cleardb.net';
-$username='bcd92b81eb6c49';
-$password='59d0b157';
-
-try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=heroku_a7483908002c347",$username,$password);
-    //$dbh1 = new PDO("mysql:host=$hostname;dbname=heroku_a7483908002c347",$username,$password);
-    echo 'Connected to Database<br/>';
-    }
-catch(PDOException $e)
-    {
-    echo $e->getMessage();
-    }
+if(isset($_SESSION['id'])=="")
+{
+	header("Location:material/login.php");
+}
