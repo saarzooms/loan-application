9997af7ca4cfa0cc66d81922fc8199ae603c4c62
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-01-23 09:35:06 +0530

    Add files via upload

--- loan-calculator-server/application/controllers/Settings.php
+++ loan-calculator-server/application/controllers/Settings.php
@@ -69,6 +69,28 @@ class Settings extends CI_Controller {
 
 		echo json_encode($data);
 	}
+	public function getClosingCostsTemplate()
+	{
+		$response = $this->Settings_model->getClosingCostsTemplate();
+		if($response)
+			$data = array('status' => 200, 'data' => $response);
+		else
+			$data = array('status' => 404, 'data' => null);
+
+		echo json_encode($data);
+	}
+	
+	public function saveClosingCostsTemplate()
+	{
+		$post = json_decode(file_get_contents('php://input'), true);
+		$data = $this->Settings_model->saveClosingCostsTemplate($post);
+		if($data)
+			$data = array('status' => 200, 'data' => $data);
+		else
+			$data = array('status' => 404, 'data' => null);
+
+		echo json_encode($data);
+	}
 
 	public function saveClosingCosts()
 	{
@@ -82,4 +104,26 @@ class Settings extends CI_Controller {
 
 		echo json_encode($data);
 	}
+	public function getClosingCostTypes(){
+		$cc = $this->Settings_model->getClosingCostTypes();
+		if($cc)
+			$data = array('status' => 200, 'data' => $cc);
+		else
+			$data = array('status' => 404, 'data' => null);
+
+		echo json_encode($data);
+	}
+	public function saveClosingCostsType()
+	{
+		$post = json_decode(file_get_contents('php://input'), true);
+
+		$data = $this->Settings_model->saveClosingCostsType($post);
+		if($data)
+			$data = array('status' => 200, 'data' => $data);
+		else
+			$data = array('status' => 404, 'data' => null);
+
+		echo json_encode($data);
+	}
+	
 }
