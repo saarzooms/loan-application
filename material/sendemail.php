1866f7de5005432b4c442e0807da7f574ba24343
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-02-28 15:46:03 +0530

    update

---
+++ material/sendemail.php
@@ -0,0 +1,58 @@
+<?php
+
+include('db/connection.php');
+require('phpmailer/class.phpmailer.php');
+
+if(isset($_POST['id']))
+{
+	$loanid=$_POST['id'];$cusid='';
+	$sql="select id from borrowers where loanid='$loanid'";
+	foreach($dbh->query($sql) as $row){
+		$cusid=$row['id'];
+	}
+	//file_put_contents('./log_'.date("j.n.Y").'.txt', $cusid, FILE_APPEND);
+	$sql="select customer_master.email,email_master.* from customer_master inner join email_master on email_master.loanofficer_id=customer_master.loanofficer_id where customer_master.id='$cusid' and email_master.template='2'";
+	$from='';$to='';$sub='';$msg='';$sendernm='';$url='';$loanofficer_id='';
+	foreach($dbh->query($sql) as $r){
+		$from=$r['sender_email'];
+		$sendernm=$r['sender_name'];
+		$sub=$r['subject'];
+		$msg=$r['message'];
+		$to=$r['email'];
+	}
+	
+	$msg=explode('{URL}',$msg);
+	$msg=$msg[0].'<br/><a href="https://loanapp-app.herokuapp.com/material/calculator.php#/loan-calculator/'.$loanid.'" target="_blank">Loan Calculator</a>';
+	
+	
+	$mail = new PHPMailer();
+	$subject = $sub;
+	$content = $msg;
+	$mail->IsSMTP();
+	$mail->SMTPDebug = 0;
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = "tls";
+	$mail->Port     = 587;  
+	//$mail->Username = "postmaster@sandbox89f874254d224787840918e8bf9a804f.mailgun.org";
+	//$mail->Password = "31527ba1cdd587c4bc2e93d7ca3a3240";
+	$mail->Username = "info@allysoftsolutions.com";
+	$mail->Password = "@llY07890";
+	//$mail->Host     = "localhost";
+	//$mail->Host     = "smtp.mailgun.org";
+	//$mail->Host     = "smtp.sendgrid.net";
+	//$mail->Host     = "smtp.gmail.com";
+	$mail->Host     = "sg2plcpnl0033.prod.sin2.secureserver.net";
+	$mail->Mailer   = "smtp";
+	$mail->SetFrom($from, $sendernm);
+	//$mail->AddReplyTo("vincy@phppot.com", "PHPPot");
+	$mail->AddAddress($to);
+	$mail->Subject = $sub;
+	$mail->WordWrap   = 80;
+	$mail->MsgHTML($content);
+	$mail->IsHTML(true);
+	if(!$mail->Send()) 
+		echo "Problem on sending mail";
+	else 
+	echo "Mail Sent Successfully !!!";
+}
+?>
