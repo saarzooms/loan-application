3d93559bd18ac259600959788776599b2a82596d
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-02-28 12:06:04 +0530

    update

--- material/js/customer.js
+++ material/js/customer.js
@@ -160,18 +160,18 @@ $(document).ready(function() {
 			
 		});
 		
-		$(document).on("click",".btn_email",function(){
-			var cusid=$(this).attr('value');
-			$.ajax({ 
-					type: "POST",
-					url: "ajax/customer_data.php",
-					data:{'cusid':cusid},
-					success: function(data)
-					{
-						$().toastmessage('showSuccessToast',data);
-					}
-			});
-		});
+		// $(document).on("click",".btn_email",function(){
+			// var cusid=$(this).attr('value');
+			// $.ajax({ 
+					// type: "POST",
+					// url: "ajax/customer_data.php",
+					// data:{'cusid':cusid},
+					// success: function(data)
+					// {
+						// $().toastmessage('showSuccessToast',data);
+					// }
+			// });
+		// });
 
 });
 function loadcustomer()
