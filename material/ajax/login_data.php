de0cb2426c086761f58afb72ca8a975f28e6c6bd
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-03-21 13:50:24 +0530

    update

--- material/ajax/login_data.php
+++ material/ajax/login_data.php
@@ -28,7 +28,7 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['pass']))
 			//$enddt=strtotime($enddt);
 //			if(strtotime($todaydt)>strtotime($enddt)){
 	//echo $enddt.' '.$todaydt;
-		/*	if($todaydt>$enddt){
+			if($todaydt>$enddt){
 				$active=0;
 					$subscription =Stripe_Subscription::all(
 					  array('customer'=>$cusid,'limit'=>1000)
@@ -56,7 +56,7 @@ if(isset($_REQUEST['username']) && isset($_REQUEST['pass']))
 					}
 					//echo $subscription['data'][0]['id'];
 					
-			}*/	
+			}
 		} 
 	}
 	if($id=='')
